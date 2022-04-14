e4cfcc74e8dcf61f5b33620b5da9411388f9b411
Szymon Jakubaszek <235879@edu.p.lodz.pl>
2022-04-14 19:54:49 +0200

    2. Formatowanie kodu zrodlowego, odpowiedz na dwa pytania w pliku HanoiRhymer.java.

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,16 +2,23 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }
+
+// Pytanie: Które wiersze w klasie HanoiRhymer były źle sformatowane?
+// Odpowiedź: Wiersze: 12,13,14,15 -> ciało funkcji public void countIn(int in)
+//            Wiersze: 5 -> zmienna int totalRejected=0;
+
+// Pytanie: Zweryfikuj działanie kombinacji klawiszy alt + ← oraz alt + →.
+// Odpowiedź: Odpowiada za przechodzenie pomiędzy otwartymi oknami.
