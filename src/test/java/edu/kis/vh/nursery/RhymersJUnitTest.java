35700a55bd73238a22466c9a6f8ee325f95d0c46
235879 <235879@CARBON.CTI.P.LODZ.PL>
2022-04-28 11:38:53 +0200

    17. Potencialne miejsca naprawy w testach.java.

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -103,3 +103,5 @@ public class RhymersJUnitTest {
     }
 
 }
+
+// TODO: Podział testów na poszczególne testujące klasy np. DefaultCountingOutRhymerJUnitTests, FIFORhymerJUnitTests, HanoiJUnitTests`
