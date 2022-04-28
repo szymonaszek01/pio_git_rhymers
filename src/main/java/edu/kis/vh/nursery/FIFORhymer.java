af9b96d2bcdbfc050bc51f995c3708aa4def0fd9
235879 <235879@CARBON.CTI.P.LODZ.PL>
2022-04-28 11:30:50 +0200

    16. Wygenerowanie szkieletu dokumentacji do klasy FIFORhymers.java.

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,16 @@
 package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
+    /**
+     * Klasa FIFORhymers dziedziczy po klasie DefaultCountingOutRhymer.
+     * Klasa FIFORhymers posiada dwie metody publiczne countOut() i getTemp().
+     **/
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+
+    /**
+     * Metoda countOut() jest nadpisana i zwraca pierwszy element ze szczytu stosu.
+     **/
 
     @Override
     public int countOut() {
@@ -18,6 +26,10 @@ public class FIFORhymer extends DefaultCountingOutRhymer {
 
         return ret;
     }
+
+    /**
+     * Metoda getTemp() zwraca utworzony stos.
+     **/
 
     public DefaultCountingOutRhymer getTemp() {
         return temp;
